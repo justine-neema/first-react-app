32a2e3bc4b1349164cf4eeee9602108bdcc1923c
justine-neema <j.neema@alustudent.com>
2026-09-11 09:29:57 +0200

    fix: adding uncalled class name

--- my-first-react-app/src/MyComponent.tsx
+++ my-first-react-app/src/MyComponent.tsx
@@ -3,7 +3,7 @@ import './MyComponent.css'; //importing the css file
 
 const MyComponent: React.FC = () => {
     return (
-        <div>
+        <div className="my-component">
             <h1> Hello from MyComponent!</h1>
             <p>This is a paragraph of text within my component.</p>
         </div>
